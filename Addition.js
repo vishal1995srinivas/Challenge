b04b4eef24bf4f1897e7761f2c77b54d74cfc9c5
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-05-16 00:10:54 +0530

    commenting

--- Addition.js
+++ Addition.js
@@ -1,9 +1,13 @@
+//In this problem, first we shud read input from the file, the first line is no of rows.
+
 const fs = require('fs');
+//Reading line
 const readline = require('readline');
 async function processLineByLine() {
 	let draftArray = [];
-	let NoOfLevels;
-	const fileStream = fs.createReadStream('INPUT.TXT');
+	//Draft Array is the array we will store our values in from the file.
+	let NoOfLevels; // Indicates the depth
+	const fileStream = fs.createReadStream('INPUT.TXT'); // Reading input
 	const rl = readline.createInterface({
 		input: fileStream,
 		crlfDelay: Infinity
@@ -11,37 +15,50 @@ async function processLineByLine() {
 	for await (let line of rl) {
 		// Each line in input.txt will be successively available here as `line`.
 		line = line.split(' ');
+		//Splitting coz input contains space, so replacing space to comma separated.
 
 		draftArray.push(line);
+		//pushing value to our arrray.
 		//console.log(`Line from file: ${line}`);
 	}
-
-	console.log('The given array is ', draftArray);
+	console.log('The given array is ', draftArray); //Displaying...
 	NoOfLevels = draftArray[0][0];
-	let ArrayOfGreaterNumbers = [];
-	let Total = 0;
+	//We know that first line indicates depth
+	let ArrayOfGreaterNumbers = []; // This is our array to be displayed after our operation
+	let Total = 0; // This is our total sum to be displayed after our operation.
 	function SumOfRightDiagnolElements() {
 		let sum = 0;
-		let finalNumbers = [];
+		let finalNumbers = []; //Indicates nos which adds up to sum.
+		//Pseudo code :
+		// observing the pattern 1[0] + 2[1] + 3[2] + 4[3];
+		// 1st array has 1 number, 2nd has 2 , 3rd has 3 , 4th  has 4. -> (array.length -1) pattern identified.
 		for (let j = 1; j < NoOfLevels; j++) {
+			//Our approach is length -1 so j is initiated to 1.
 			let tempSum = 0,
 				TempNumbers = [];
 			for (let i = 1; i <= NoOfLevels; i++) {
+				//Array incrementing till last level
 				let length = draftArray[i].length;
 				let diff = length - j;
+				//whenever diff becomes negative we get NaN , this occurs when length < j , in that case we need first element
 				if (diff < 0) {
 					diff = 0;
 				}
-				let a = parseInt(draftArray[i][diff]);
-				tempSum += a;
-				TempNumbers.push(a);
-				//console.log(a, length);
+
+				let RequiredNumber = parseInt(draftArray[i][diff]);
+				//Add to sum
+				tempSum += RequiredNumber;
+
+				TempNumbers.push(RequiredNumber);
+				//console.log(RequiredNumber, length);
 			}
+			//Checking whether this is the greatest within this rightdiagnol method
 			if (sum < tempSum) {
 				sum = tempSum;
 				finalNumbers = [ ...TempNumbers ];
 			}
 		}
+		//Competing with left diagnol
 		if (Total < sum) {
 			Total = sum;
 			ArrayOfGreaterNumbers = [ ...finalNumbers ];
@@ -85,7 +102,6 @@ async function processLineByLine() {
 			ArrayOfGreaterNumbers = [ ...finalNumbers ];
 		}
 	}
-
 	try {
 		let leftDiagnolRunFirst = await SumOfLeftDiagnolElements();
 		let RightDiagnolNow = await SumOfRightDiagnolElements();
@@ -94,3 +110,6 @@ async function processLineByLine() {
 	}
 }
 processLineByLine();
+//
+//Ensure you download with Input text file. and then
+//Run command in terminal : node Addition
